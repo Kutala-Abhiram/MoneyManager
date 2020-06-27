994909aab6ca814fd6d1d099c8449de0c1cad1c5
Kutala-Abhiram <kabhiram1998@gmail.com>
2020-06-27 18:01:24 +0530

    set the password

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'kabhiram1998@gmail.com', password: '12hi@1756', password_confirmation: '12hi@1756')
